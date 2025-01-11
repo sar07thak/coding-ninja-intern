c81bcbfe86e404c9bc9a8f99f415e44b593c0be4
sarthak <sarthak75220@gmail.com>
2025-01-11 14:26:37 +0530

    Update package dependencies and enhance SliderCompo component: Added new libraries (react-slick, slick-carousel, swiper) for improved slider functionality, removed the unused Profile.js file, and updated the SliderCompo to utilize the new slider features with enhanced layout and styling.

---
+++ vite-project/src/Api/Profiles.js
@@ -0,0 +1,93 @@
+// -----------UBER--------- //
+import uberProfile from "../assets/sonu_uber.png";
+import uber from "../assets/uber.png";
+
+// -----------AWS--------- //
+
+import awsProfile from "../assets/yash_aws.png";
+import aws from "../assets/amazon-web-service.png";
+
+// -----------GOOGLE--------- //
+import googleProfile from "../assets/kashish_google.png";
+import google from "../assets/google.png";
+
+// -----------PERSISTE--------- //
+import persisteProfile from "../assets/pradeep_persistent.png";
+import persiste from "../assets/persistent.png";
+
+//------------MICROSOFT-----------//
+import microsoftProfile from "../assets/shourya_microsoft.png";
+import microsoft from "../assets/microsoft.png";
+
+//------------PAYPAL-----------//
+import paypalProfile from "../assets/amitesh_paypal.png";
+import paypal from "../assets/paypal.png";
+
+//------------CALSOFT-----------//
+import calsoftProfile from "../assets/aikansh_calsoft.png";
+import calsoft from "../assets/calsoft.png";
+
+import optumProfile from "../assets/naveen_optum.png";
+import optum from "../assets/optum.png";
+
+import citibankProfile from "../assets/ruchika_citi_bank.png";
+import citibank from "../assets/citibank.png";
+
+
+
+export const data = [
+  {
+    id: 1,
+    college: "tier 2/3 college",
+    profilePhoto: [uberProfile],
+    companyPhoto: [uber],
+  },
+  {
+    id: 2,
+    college: "service to product",
+    profilePhoto: [awsProfile],
+    companyPhoto: [aws],
+  },
+  {
+    id: 3,
+    college: "tier 2/3 college",
+    profilePhoto: [persisteProfile],
+    companyPhoto: [persiste],
+  },
+  {
+    id: 4,
+    college: "CS/IT",
+    profilePhoto: [paypalProfile],
+    companyPhoto: [paypal],
+  },
+  {
+    id: 5,
+    college: "service to product",
+    profilePhoto: [calsoftProfile],
+    companyPhoto: [calsoft],
+  },
+  {
+    id: 6,
+    college: "CS/IT",
+    profilePhoto: [googleProfile],
+    companyPhoto: [google],
+  },
+  {
+    id: 7,
+    college: "CS/IT",
+    profilePhoto: [microsoftProfile],
+    companyPhoto: [microsoft],
+  },
+  {
+    id: 8,
+    college: "non - CS/IT",
+    profilePhoto: [citibankProfile],
+    companyPhoto: [citibank],
+  },
+  {
+    id: 7,
+    college: "non-CS/IT",
+    profilePhoto: [optumProfile],
+    companyPhoto: [optum],
+  },
+];
--- vite-project/src/components/SliderCompo.jsx
+++ vite-project/src/components/SliderCompo.jsx
@@ -1,18 +1,60 @@
-import React from 'react'
-import frame from "../assets/frame-body.jpg"
+import React, { useEffect, useRef, useState } from "react";
+import frame from "../assets/frame-body.jpg";
+import { data } from "../Api/Profiles.js";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
 export const SliderCompo = () => {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 600,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+    swipeToSlide: true,
+    centerMode: true,
+    centerPadding: "30px",
+  };
+
+
+
   return (
     <>
-    <div className='w-full  border border-red-400 bg-[#FFFFFF] flex flex-col gap-20 '>
-        <div className='border border-green-500 w-full  bg-red-200 flex justify-center items-center px-40'>
-            <div className='border border-red-700 w-full h-52 ' >
-
-            </div>
+      <div className="w-full  bg-[#FFFFFF]">
+        <div className="mt-16 w-full  h-[500px] px-24">
+          <div className="font-bold text-xl text-center mb-10">Our Ninjas at top Companies</div>
+          <div className="cursor-pointer ">
+            <Slider {...settings}>
+              {data.map((currentElement, index) => (
+                <div key={index} className="flex flex-col w-20 justify-center items-center " >
+                  <div className="m-2 p-5 rounded-2xl shadow-gray-300 shadow-2xl">
+                  <img
+                    src={currentElement.profilePhoto[0]}
+                    alt={`Profile ${index + 1}`}
+                    className="m-auto h-32 w-32"
+                  />
+                  <p className="font-semibold text-md text-center text-[#9A9B9C] ">{currentElement.college}</p>
+                  <img
+                    src={currentElement.companyPhoto[0]}
+                    alt={`Company ${index + 1}`}
+                    className=" h-28  w-28 m-auto"
+                  />
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
         </div>
-        <div className='w-full '>
-            <img src={frame} alt="" />
+        <div className="w-full">
+          <img src={frame} alt="" />
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
